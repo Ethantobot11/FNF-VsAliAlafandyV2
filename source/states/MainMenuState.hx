d0ca1fa21e9ac340cd3399a4b1912f08f930f5fb
AliAlafandy <alialafandy913@gmail.com>
2026-03-07 11:13:28 +0200

    Initial commit

---
+++ source/states/MainMenuState.hx
@@ -0,0 +1,271 @@
+package states;
+
+import flixel.FlxObject;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.effects.FlxFlicker;
+import lime.app.Application;
+import states.editors.MasterEditorMenu;
+import options.OptionsState;
+
+class MainMenuState extends MusicBeatState
+{
+	public static var modsEngineVersion:String = '6.9.9'; // This is also used for Discord RPC
+	public static var funkinVersion:String = '0.2.8';
+	public static var curSelected:Int = 0;
+
+	var menuItems:FlxTypedGroup<FlxSprite>;
+
+	var optionShit:Array<String> = [
+		'story_mode',
+		'freeplay',
+		
+		#if ACHIEVEMENTS_ALLOWED
+		'awards',
+		#end
+
+		'gallery',
+		'credits',
+		
+		#if !switch
+		'socials',
+		#end
+		
+		'options'
+	];
+
+	var magenta:FlxSprite;
+	var camFollow:FlxObject;
+
+	var char6:FlxSprite;
+
+	override function create()
+	{
+		#if MODS_ALLOWED
+		Mods.pushGlobalMods();
+		#end
+		Mods.loadTopMod();
+
+		#if DISCORD_ALLOWED
+		// Updating Discord Rich Presence
+		DiscordClient.changePresence("In the Menus", null);
+		#end
+
+		transIn = FlxTransitionableState.defaultTransIn;
+		transOut = FlxTransitionableState.defaultTransOut;
+
+		persistentUpdate = persistentDraw = true;
+
+		var yScroll:Float = Math.max(0.25 - (0.05 * (optionShit.length - 4)), 0.1);
+		var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('menuDesat'));
+		bg.antialiasing = ClientPrefs.data.antialiasing;
+		bg.scrollFactor.set(0, yScroll);
+		bg.setGraphicSize(Std.int(bg.width * 1.175));
+		bg.updateHitbox();
+		bg.screenCenter();
+		bg.color = 0xFF000080;
+		add(bg);
+
+		camFollow = new FlxObject(0, 0, 1, 1);
+		add(camFollow);
+
+		magenta = new FlxSprite(-80).loadGraphic(Paths.image('menuDesat'));
+		magenta.antialiasing = ClientPrefs.data.antialiasing;
+		magenta.scrollFactor.set(0, yScroll);
+		magenta.setGraphicSize(Std.int(magenta.width * 1.175));
+		magenta.updateHitbox();
+		magenta.screenCenter();
+		magenta.visible = false;
+		magenta.color = 0xFF00FFFF;
+		add(magenta);
+
+		menuItems = new FlxTypedGroup<FlxSprite>();
+		add(menuItems);
+
+		for (i in 0...optionShit.length)
+		{
+			var offset:Float = 108 - (Math.max(optionShit.length, 4) - 4) * 80;
+			var menuItem:FlxSprite = new FlxSprite(0, (i * 140) + offset);
+			menuItem.antialiasing = ClientPrefs.data.antialiasing;
+			menuItem.frames = Paths.getSparrowAtlas('mainmenu/menu_' + optionShit[i]);
+			menuItem.animation.addByPrefix('idle', optionShit[i] + " basic", 24);
+			menuItem.animation.addByPrefix('selected', optionShit[i] + " white", 24);
+			menuItem.animation.play('idle');
+			menuItems.add(menuItem);
+			var scr:Float = (optionShit.length - 4) * 0.135;
+			if (optionShit.length < 6)
+				scr = 0;
+			menuItem.scrollFactor.set(0, scr);
+			menuItem.updateHitbox();
+			// menuItem.screenCenter(X);
+			menuItem.x = 95;
+		}
+
+		char6 = new FlxSprite(415, 500);
+		char6.antialiasing = ClientPrefs.data.antialiasing;
+		char6.frames = Paths.getSparrowAtlas('mainmenu/menu_characters/options_thinking');
+		char6.animation.addByPrefix('menu', "Menu", 24);
+		char6.animation.play('menu');
+		char6.updateHitbox();
+		char6.visible = false;
+		// char6.setGraphicSize(Std.int(char6.width * 1));
+		add(char6);
+
+		var modVer:FlxText = new FlxText(12, FlxG.height - 64, 0, "Vs Ali Alafandy v" + Application.current.meta.get('version'), 12);
+		modVer.scrollFactor.set();
+		modVer.setFormat("VCR OSD Mono", 16, FlxColor.BLUE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		add(modVer);
+		var modsVer:FlxText = new FlxText(12, FlxG.height - 44, 0, "Mods Engine v" + modsEngineVersion, 12);
+		modsVer.scrollFactor.set();
+		modsVer.setFormat("VCR OSD Mono", 16, FlxColor.BLUE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		add(modsVer);
+		var fnfVer:FlxText = new FlxText(12, FlxG.height - 24, 0, "Friday Night Funkin' v" + funkinVersion, 12);
+		fnfVer.scrollFactor.set();
+		fnfVer.setFormat("VCR OSD Mono", 16, FlxColor.BLUE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		add(fnfVer);
+		changeItem();
+
+		#if ACHIEVEMENTS_ALLOWED
+		// Unlocks "Freaky on a Friday Night" achievement if it's a Friday and between 18:00 PM and 23:59 PM
+		var leDate = Date.now();
+		if (leDate.getDay() == 5 && leDate.getHours() >= 18)
+			Achievements.unlock('friday_night_play');
+
+		#if MODS_ALLOWED
+		Achievements.reloadList();
+		#end
+		#end
+
+		#if mobile
+		addTouchPad("UP_DOWN", "A_B");
+		#end
+
+		super.create();
+
+		FlxG.camera.follow(camFollow, null, 9);
+	}
+
+	var selectedSomethin:Bool = false;
+
+	override function update(elapsed:Float)
+	{
+		if (FlxG.sound.music.volume < 0.8)
+		{
+			FlxG.sound.music.volume += 0.5 * elapsed;
+			if (FreeplayState.vocals != null)
+				FreeplayState.vocals.volume += 0.5 * elapsed;
+		}
+
+		if (optionShit[curSelected] == 'options')
+		{
+			char6.visible = true;
+		}
+		else
+		{
+			char6.visible = false;
+		}
+
+		if (!selectedSomethin)
+		{
+			if (controls.UI_UP_P)
+				changeItem(-1);
+
+			if (controls.UI_DOWN_P)
+				changeItem(1);
+
+			if (controls.BACK)
+			{
+				selectedSomethin = true;
+				FlxG.sound.play(Paths.sound('cancelMenu'));
+				MusicBeatState.switchState(new TitleState());
+			}
+
+			if (controls.ACCEPT)
+			{
+				FlxG.sound.play(Paths.sound('confirmMenu'));
+				if (optionShit[curSelected] == 'socials')
+				{
+					CoolUtil.browserLoad('https://linktr.ee/vsalialafandy');
+				}
+				else
+				{
+					selectedSomethin = true;
+
+					if (ClientPrefs.data.flashing)
+						FlxFlicker.flicker(magenta, 1.1, 0.15, false);
+
+					FlxFlicker.flicker(menuItems.members[curSelected], 1, 0.06, false, false, function(flick:FlxFlicker)
+					{
+						switch (optionShit[curSelected])
+						{
+							case 'story_mode':
+								MusicBeatState.switchState(new StoryMenuState());
+							case 'freeplay':
+								MusicBeatState.switchState(new FreeplayState());
+
+							#if MODS_ALLOWED
+							case 'mods':
+								MusicBeatState.switchState(new ModsMenuState());
+							#end
+
+							#if ACHIEVEMENTS_ALLOWED
+							case 'awards':
+								MusicBeatState.switchState(new AchievementsMenuState());
+							#end
+
+							case 'gallery':
+								MusicBeatState.switchState(new GalleryState());
+							case 'credits':
+								MusicBeatState.switchState(new CreditsState());
+							case 'options':
+								MusicBeatState.switchState(new OptionsState());
+								OptionsState.onPlayState = false;
+								if (PlayState.SONG != null)
+								{
+									PlayState.SONG.arrowSkin = null;
+									PlayState.SONG.splashSkin = null;
+									PlayState.stageUI = 'normal';
+								}
+						}
+					});
+
+					for (i in 0...menuItems.members.length)
+					{
+						if (i == curSelected)
+							continue;
+						FlxTween.tween(menuItems.members[i], {alpha: 0}, 0.4, {
+							ease: FlxEase.quadOut,
+							onComplete: function(twn:FlxTween)
+							{
+								menuItems.members[i].kill();
+							}
+						});
+					}
+				}
+			}
+		}
+
+		super.update(elapsed);
+	}
+
+	function changeItem(huh:Int = 0)
+	{
+		FlxG.sound.play(Paths.sound('scrollMenu'));
+		menuItems.members[curSelected].animation.play('idle');
+		menuItems.members[curSelected].updateHitbox();
+		// menuItems.members[curSelected].screenCenter(X);
+
+		curSelected += huh;
+
+		if (curSelected >= menuItems.length)
+			curSelected = 0;
+		if (curSelected < 0)
+			curSelected = menuItems.length - 1;
+
+		menuItems.members[curSelected].animation.play('selected');
+		menuItems.members[curSelected].centerOffsets();
+		// menuItems.members[curSelected].screenCenter(X);
+
+		camFollow.setPosition(menuItems.members[curSelected].getGraphicMidpoint().x,
+			menuItems.members[curSelected].getGraphicMidpoint().y - (menuItems.length > 4 ? menuItems.length * 8 : 0));
+	}
+}
---
+++ source/states/WarningState.hx
@@ -0,0 +1,187 @@
+package states;
+
+import flixel.FlxSubState;
+import flixel.FlxG;
+
+import flixel.text.FlxText;
+import flixel.effects.FlxFlicker;
+import lime.app.Application;
+import flixel.addons.display.FlxBackdrop;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.addons.transition.FlxTransitionableState;
+
+class WarningState extends MusicBeatState
+{
+	public static var leftState:Bool = false;
+
+	var bg:FlxSprite;
+
+	#if mobile
+	var warnTextMobile:FlxText;
+	#else
+	var warnText:FlxText;
+	#end
+	
+	override function create()
+	{
+		super.create();
+		
+		bg = new FlxSprite().loadGraphic(Paths.image("menuDesat"));
+		bg.antialiasing = ClientPrefs.data.antialiasing;
+		bg.color = 0xFF000080;
+		bg.updateHitbox();
+		bg.screenCenter();
+		add(bg);
+		
+		var grid:FlxBackdrop = new FlxBackdrop(FlxGridOverlay.createGrid(80, 80, 160, 160, true, 0x330000FF, 0x0));
+		grid.velocity.set(40, 40);
+		grid.alpha = 0;
+		FlxTween.tween(grid, {alpha: 1}, 0.5, {ease: FlxEase.quadOut});
+		add(grid);
+		
+		var ramGB:Float = Math.round(getSystemRAM() * 100) / 100;
+
+		#if mobile
+		var warnMobile:String = "Hey, yoo!!\n" +
+			"This Mod needs at least 4 GB of RAM to run nicely.\n" +
+			"System Detected RAM: " + ramGB + " GB.\n" +
+			"Also,\n" +
+			"This Mod contains some flashing lights!\n" +
+			"Press A to continue and disable them now or go to Options Menu.\n" +
+			"Press B to ignore this massage.\n" +
+			"You've been warned!";
+
+		var guhMobile:String = "Hey, yoo!!\n
+			This Mod contains some flashing lights!\n
+			Press A to continue and disable them now or go to Options Menu.\n
+			Press B to ignore this massage.\n
+			You've been warned!";
+		#else
+		var warn:String = "Hey, yoo!!\n" +
+			"This Mod needs at least 4 GB of RAM to run nicely.\n" +
+			"System Detected RAM: " + ramGB + " GB.\n" +
+			"Also,\n" +
+			"This Mod contains some flashing lights!\n" +
+			"Press ENTER to continue and disable them now or go to Options Menu.\n" +
+			"Press ESCAPE to ignore this massage.\n" +
+			"You've been warned!";
+
+		var guh:String = "Hey, yoo!!\n
+			This Mod contains some flashing lights!\n
+			Press ENTER to continue and disable them now or go to Options Menu.\n
+			Press ESCAPE to ignore this massage.\n
+			You've been warned!";
+		#end
+		
+		if (ramGB < 3.49)
+		{
+			#if mobile
+			warnTextMobile = new FlxText(0, 0, FlxG.width, warnMobile, 32);
+			warnTextMobile.setFormat("VCR OSD Mono", 32, FlxColor.CYAN, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+			warnTextMobile.screenCenter(Y);
+			add(warnTextMobile);
+			#else
+			warnText = new FlxText(0, 0, FlxG.width, warn, 32);
+			warnText.setFormat("VCR OSD Mono", 32, FlxColor.CYAN, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+			warnText.screenCenter(Y);
+			add(warnText);
+			#end
+			
+			trace('Warning: System has less than 4 GB of RAM. Detected RAM: ' + ramGB + ' GB');
+		} else {
+			#if mobile
+			warnTextMobile = new FlxText(0, 0, FlxG.width, guhMobile, 32);
+			warnTextMobile.setFormat("VCR OSD Mono", 32, FlxColor.CYAN, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+			warnTextMobile.screenCenter(Y);
+			add(warnTextMobile);
+			#else
+			warnText = new FlxText(0, 0, FlxG.width, guh, 32);
+			warnText.setFormat("VCR OSD Mono", 32, FlxColor.CYAN, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+			warnText.screenCenter(Y);
+			add(warnText);
+			#end
+			
+			trace('System has sufficient RAM: ' + ramGB + ' GB');
+		}
+		
+		#if mobile
+		addTouchPad("NONE", "A_B");
+		#end
+	}
+	
+	override function update(elapsed:Float)
+	{
+		if(!leftState) {
+			var back:Bool = controls.BACK;
+			if (controls.ACCEPT || back) {
+				leftState = true;
+				FlxTransitionableState.skipNextTransIn = true;
+				FlxTransitionableState.skipNextTransOut = true;
+				if(!back) {
+					ClientPrefs.data.flashing = false;
+					ClientPrefs.saveSettings();
+					FlxG.sound.play(Paths.sound('confirmMenu'));
+
+					#if mobile
+					FlxFlicker.flicker(warnTextMobile, 1, 0.1, false, true, function(flk:FlxFlicker) {
+						new FlxTimer().start(0.5, function (tmr:FlxTimer) {
+							MusicBeatState.switchState(new TitleState());
+						});
+					});
+					#else
+					FlxFlicker.flicker(warnText, 1, 0.1, false, true, function(flk:FlxFlicker) {
+						new FlxTimer().start(0.5, function (tmr:FlxTimer) {
+							MusicBeatState.switchState(new TitleState());
+						});
+					});
+					#end
+				} else {
+					FlxG.sound.play(Paths.sound('cancelMenu'));
+
+					#if mobile
+					FlxTween.tween(warnTextMobile, {alpha: 0}, 1, {
+						onComplete: function (twn:FlxTween) {
+							MusicBeatState.switchState(new TitleState());
+						}
+					});
+					#else
+					FlxTween.tween(warnText, {alpha: 0}, 1, {
+						onComplete: function (twn:FlxTween) {
+							MusicBeatState.switchState(new TitleState());
+						}
+					});
+					#end
+				}
+			}
+		}
+		super.update(elapsed);
+	}
+	
+	function getSystemRAM():Float {
+		#if windows
+		var process = new Process("wmic", ["OS", "get", "TotalVisibleMemorySize"]);
+		var output = process.stdout.readAll().toString();
+		process.close();
+		
+		var lines = output.split("\n");
+		for (line in lines) {
+		var trimmed = StringTools.trim(line);
+		if (~/^\d+$/.match(trimmed)) {
+			return Std.parseInt(trimmed) / (1024 * 1024); 
+		}
+	}
+	#elseif linux
+	var process = new Process("grep", ["MemTotal", "/proc/meminfo"]);
+	var output = process.stdout.readAll().toString();
+	process.close();
+	
+	var parts = output.split(":");
+	if (parts.length > 1) {
+		var kbRam = Std.parseInt(parts[1].replace("kB", "").trim());
+		return kbRam / (1024 * 1024);
+	}
+	#end
+	
+	return 0;
+	}
+}
