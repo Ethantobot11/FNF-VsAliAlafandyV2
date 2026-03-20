155ecfb219de569889a0ad02dca55dbeef1f99e0
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-03-20 05:34:38 +0200

    Update TitleState.hx

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -296,7 +296,7 @@ class TitleState extends MusicBeatState
 
 		credTextShit.visible = false;
 
-		alafandyLogo = new FlxSprite(0, -20).loadGraphic(Paths.image('alafandy_logo'));
+		alafandyLogo = new FlxSprite(0, -5).loadGraphic(Paths.image('alafandy_logo'));
 		add(alafandyLogo);
 		alafandyLogo.visible = false;
 		alafandyLogo.setGraphicSize(Std.int(alafandyLogo.width * 0.8));
